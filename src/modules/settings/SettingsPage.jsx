7c28ce5b04b0fa00ace5a00b2b0424b6a48fe63a
Krishna <kstanwar0000@gmail.com>
2026-03-01 21:12:07 +0530

    refactor(settings): consolidate tabs into settings module for modular architecture

---
+++ src/modules/settings/SettingsPage.jsx
@@ -0,0 +1,7 @@
+import AIModelTab from "./tabs/AIModelTab";
+import SecurityTab from "./tabs/SecurityTab";
+import MemoryTab from "./tabs/MemoryTab";
+import IntegrationsTab from "./tabs/IntegrationsTab";
+import VoiceTab from "./tabs/VoiceTab";
+import AppearanceTab from "./tabs/AppearanceTab";
+import DeveloperTab from "./tabs/DeveloperTab";
