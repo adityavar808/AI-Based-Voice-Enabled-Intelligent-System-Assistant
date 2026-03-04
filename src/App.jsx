a71bc88c14c363e0135c09b5bbab5f74808ac6e0
adityavar808 <adityavarshney808@gmail.com>
2026-03-04 20:42:15 +0530

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Analytics } from "@vercel/analytics/react"
 import Home from "./pages/Home";
 import SettingsPage from "./modules/settings/SettingsPage";
 
