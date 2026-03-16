6dd43457cd49b6bb29c80c1b224793717bd7c312
Krishna <kstanwar0000@gmail.com>
2026-03-16 17:27:25 +0530

    Add SQLite database and user model

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from app.routes import chat, auth
+from app.database.db import engine, Base
 
 app = FastAPI()
 
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
     return {"message": "ZENIX FastAPI backend running"}
-
 
 app.include_router(chat.router)
 app.include_router(auth.router)
