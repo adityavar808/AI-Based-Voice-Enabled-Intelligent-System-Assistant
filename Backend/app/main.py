4891854e9d676d86dd5188280fbe6f25476128b4
Krishna <kstanwar0000@gmail.com>
2026-03-16 21:27:50 +0530

    Connect React frontend to FastAPI authentication APIs

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,9 +1,16 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from app.routes import chat, auth
 from app.database.db import engine, Base
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 Base.metadata.create_all(bind=engine)
 
 @app.get("/")
